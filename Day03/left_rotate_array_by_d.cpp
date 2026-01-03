cb65c714964f16c9f9186e98c8da0383a0647b13
Aravind-2508 <aravindabramaina@gmail.com>
2026-01-03 19:46:55 +0530

    optimal for array rotation

--- Day03/left_rotate_array_by_d.cpp
+++ Day03/left_rotate_array_by_d.cpp
@@ -1,31 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-
+// Problem Link: https://leetcode.com/problems/rotate-array/
 class Solution {
 public:
-    void rotateArrayByOne(vector<int>& nums) {
-        // Store the first element in a temporary variable
-        int temp = nums[0];
+    void rotate(vector<int>& nums, int k) {
+        int d = k%nums.size();
+        reverse(nums.begin(), nums.end()-d);
+        reverse(nums.end()-d,nums.end());
+        reverse(nums.begin(),nums.end());
         
-        // Shift elements to the left
-        for (int i = 1; i < nums.size(); ++i) {
-            nums[i - 1] = nums[i];
-        }
-
-        // Place the first element at the end
-        nums[nums.size() - 1] = temp;
+        
     }
 };
-
-int main() {
-    Solution solution;
-    vector<int> nums = {1, 2, 3, 4, 5};
-
-    solution.rotateArrayByOne(nums);
-
-    for (int num : nums) {
-        cout << num << " "; // Output the rotated array
-    }
-
-    return 0;
-}
