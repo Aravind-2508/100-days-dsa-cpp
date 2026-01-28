5628ab659421d6f6082126bd466835789386da59
Aravind Abramaina <149137311+Aravind-2508@users.noreply.github.com>
2026-01-28 00:38:10 +0000

    "Day 26 is only deletion  of head and tail of the linkedin list"

---
+++ Day26/delete_tail.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int data1, node* next1){
+        data = data1;
+        next = next1;
+    }
+    node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+node* array2ll(vector<int>arr){
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i=1; i<arr.size(); i++){
+        node * temp = new node (arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+void print(node* head){
+    while(head != NULL){
+        cout<<head->data<<" ";
+        head = head -> next;
+    }
+    cout<<endl;
+}
+node* removeshead(node* head){
+    if(head == NULL) return head;
+    node* temp = head;
+    head = head -> next;
+    //free(temp);
+    delete temp;
+    return head;
+}
+node* delete_tail(node * head){
+    if(head == NULL || head -> next == NULL) return NULL;
+    node* temp = head;
+    while(temp -> next -> next != NULL){
+        temp = temp -> next;
+    }
+    free(temp -> next);
+    temp -> next = nullptr;
+    
+    return head;
+}
+
+int main(){
+    vector<int>arr={1,2,3,4};
+    node* head = array2ll(arr);
+    head = removeshead(head);
+    head = delete_tail(head);
+    print(head);
+}
