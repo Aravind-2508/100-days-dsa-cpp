5f1eb612bd0b2297e8e67c181375aa0105ee3155
Aravind Abramaina <149137311+Aravind-2508@users.noreply.github.com>
2026-02-12 16:13:11 +0000

    "detect and return starting point of ll"

---
+++ Day43/start_node_loop_ll.cpp
@@ -0,0 +1,11 @@
+//logic is 
+/*
+node* slow = head;
+node* fast = head;
+while(fast != NULL && fast -> next!= NULL){
+    slow = slow -> next;
+    fast = fast -> next -> next;
+    if(slow == fast ) return true;
+
+}
+return false; */
